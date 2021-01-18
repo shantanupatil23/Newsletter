8a5d18dca3c8a36f03e8f50a70b41f3894b8be10
shantanupatil23 <kanhapatildurg@gmail.com>
2021-01-19 03:20:58 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Newsletter
-I've created this project while doing Web Development course on Udemy. Project No #07
+
+I've created this project while doing Web Development course on Udemy. Project No #08
+
+Technologies used - HTML, JavaScript, Express.js with Node.js, APIs
---
+++ app.js
@@ -0,0 +1,56 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: { FNAME: firstName, LNAME: lastName },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/018151b33f";
+
+  const options = {
+    method: "POST",
+    auth: "shantanu1:21de981a40a5f61d3b82745c575be1ac-us7",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      if (response.statusCode == 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000.");
+});
